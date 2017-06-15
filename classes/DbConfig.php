466da5739d09095d3edc4c3ae8355b26c6166bfb
EvgHrn <evg.hrn@gmail.com>
2017-06-15 21:04:46 +0400

    Init

--- classes/DbConfig.php
+++ classes/DbConfig.php
@@ -1,2 +1,11 @@
 <?php
 
+class DbConfig
+{
+    private $_host = 'localhost';
+    private $username = 'root';
+    private $password = 'test';
+    provate $_database ='test';
+
+    protected $connection;
+}
